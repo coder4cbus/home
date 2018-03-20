aade1d84add538c2c44d0dbda6ed8053a18206e3
coder4cbus <coder4cbus@gmail.com>
2018-03-20 00:14:20 -0400

    Email jobs to end of webpage directions. Issue with loading, suspect it is due to gem zxcvbnable, so deactivated those changes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,27 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
 #:timeoutable and :omniauthable
-devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable #:confirmable, :lockable
+devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable #:zxcvbnable #:confirmable, :lockable
 end
 #eau - no comma can be used on last symbol or there is an error message
+#below throws an error as shown:
+# rb:7:in `<top (required)>': superclass mismatch for class User (TypeError)
 
-#def send_devise_notification(notification, *args)
+# class User < ActiveRecord::Base
+#   devise :zxcvbnable
+
+  # Optionally add more weak words to check against:
+  def weak_words
+    ['mysitename', self.name, self.username]
+  end
+end
+
+def send_devise_notification(notification, *args)
 #     if Rails.env.production?
 #       # No worker process in production to handle scheduled tasks
 #       devise_mailer.send(notification, self, *args).deliver_now
 #     else
-#       devise_mailer.send(notification, self, *args).deliver_later
+      devise_mailer.send(notification, self, *args).deliver_later
 #     end
 #   end
-# end
+end
