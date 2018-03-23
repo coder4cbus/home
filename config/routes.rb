f5a5b0a911c13c5eaf98e5501c5518533faf8399
coder4cbus <coder4cbus@gmail.com>
2018-03-23 19:56:16 -0400

    Overwrite for Desired Info

--- config/routes.rb
+++ config/routes.rb
@@ -20,25 +20,73 @@ Rails.application.routes.draw do
 
     post '/users' =>     'devise/registrations#create'
 
-    get '/users/cancel' =>     'devise/registrations#cancel', as: :cancel_reg
+    # get '/users/cancel' =>     'devise/registrations#cancel', as: :cancel_reg
 
     get '/users/edit' => 'devise/registrations#edit', as: :edit_reg
 
     put '/users' => 'devise/registrations#update', as: :update_reg
 
-    get '/users' =>     'devise/registrations#destroy', as: :delete_reg
+    get '/users/destroy' =>     'devise/registrations#destroy', as: :delete_reg
 
-
+# /users page results in the following flash message so it appears two paths perform same action of registraiton cancellation
+#Bye! Your account has been successfully cancelled. We hope to see you again soon.
 
     get '/users/password/new' => 'devise/password#new', as: :new_pw
 
     post '/users/password' => 'devise/passwords#create'
 
-    get '/users/password/edit' => 'devise/passwords#edit', as: :edit_pw
+    # get '/users/password/edit' => 'devise/passwords#edit', as: :edit_pw
 
     put '/users/password' => 'devise/passwords#update', as: :update_pw
 
     end
   end
 
-  # get '/users/sign_in' => 'users#sign_in', as: :sign_in
+  # eau- routes that are not working have been deactivated
+
+  #EAU copied routes from previous app
+  # Rails.application.routes.draw do
+    # resources :users
+    # resources :clients
+    # resources :rdvms
+    # resources :pets
+    #
+    #
+    # root 'users#index'
+    #
+    # get     'users/new' =>          'users#new', as: 'new_user'
+    #
+    # get     'users' =>              'users#index', as: 'users'
+    #
+    # post    'users' =>              'users#create'
+    #
+    # get     'users/:id' =>          'users#show', as: 'user'
+    #
+    # delete  'users/:id' =>          'users#destroy'
+    #
+    # patch   'users/:id' =>          'users#update'
+    #
+    # get     'users/:id/edit' =>     'users#edit', as: 'edit_user'
+
+    # get     'login' =>               'users#new', as: 'vet_login'
+    #
+    # get     'users/new' =>          'users#new', as: 'new_vet'
+    #
+    # get     'vets' =>              'users#index', as: 'vets'
+    #
+    # post    'users' =>              'users#create'
+    #
+    # get     'users/:id' =>          'users#show', as: 'vet'
+    #
+    # delete  'users/:id' =>          'users#destroy'
+    #
+    # patch   'users/:id' =>          'users#update'
+    #
+    # get     'users/:id/edit' =>     'users#edit', as: 'edit_vet'
+
+
+
+
+    # end
+
+  # eau- routes that are not working have been deactivated
